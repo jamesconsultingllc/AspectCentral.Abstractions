e4a52126b76660c281bf7e2157d7e6dc2522ff01
Rudy James <rudy@jamesconsulting.biz>
2019-09-26 16:52:35 -0500

    Registration updates

--- AspectCentral.Abstractions/AspectRegistrationBuilder.cs
+++ AspectCentral.Abstractions/AspectRegistrationBuilder.cs
@@ -73,8 +73,17 @@ namespace AspectCentral.Abstractions
 
         private void RegisterAspectConfiguration(AspectConfiguration aspectConfiguration)
         {
-            Services.TryAdd(ServiceDescriptor.Describe(aspectConfiguration.ServiceDescriptor.ImplementationType, 
-                aspectConfiguration.ServiceDescriptor.ImplementationType, aspectConfiguration.ServiceDescriptor.Lifetime));
+            if (aspectConfiguration.ServiceDescriptor.ImplementationType == null)
+            {
+                Services.TryAdd(ServiceDescriptor.Describe(aspectConfiguration.ServiceDescriptor.ImplementationType,
+                    aspectConfiguration.ServiceDescriptor.ImplementationType, aspectConfiguration.ServiceDescriptor.Lifetime));
+            }
+            else
+            {
+                Services.TryAdd(ServiceDescriptor.Describe(aspectConfiguration.ServiceDescriptor.ImplementationType,
+                    aspectConfiguration.ServiceDescriptor.ImplementationType, aspectConfiguration.ServiceDescriptor.Lifetime));
+            }
+
             Services.Add(ServiceDescriptor.Describe(aspectConfiguration.ServiceDescriptor.ServiceType,
                 serviceProvider => InvokeCreateFactory(serviceProvider, aspectConfiguration), aspectConfiguration.ServiceDescriptor.Lifetime));
             Services.TryAdd(aspectConfiguration.ServiceDescriptor);
